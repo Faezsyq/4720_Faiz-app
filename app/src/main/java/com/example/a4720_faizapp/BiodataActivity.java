5cce92d4803fc27e522df01db66c2cfc346801bf
Faezsyq <faez.syq@gmail.com>
2023-09-18 16:56:48 +0700

    porfil

---
+++ app/src/main/java/com/example/a4720_faizapp/BiodataActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4720_faizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+}
